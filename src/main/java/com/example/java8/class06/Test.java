e71b9f790d93891f0d66e0e0cd6dcb8435c53948
SKMNS\202110043 <jeonheejin@sk.com>
2023-02-10 15:45:04 +0900

    a

---
+++ src/main/java/com/example/java8/class06/Test.java
@@ -0,0 +1,7 @@
+package com.example.java8.class06;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
--- src/test/java/com/example/java8/Java8ApplicationTests.java
+++ src/test/java/com/example/java8/Java8ApplicationTests.java
@@ -3,11 +3,23 @@ package com.example.java8;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 @SpringBootTest
 class Java8ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String a = null;
+		String s = Optional.ofNullable(a).orElse("");
+
+		System.out.println("s = " + a);
+		System.out.println("s = " + s);
+
+		int i = Integer.parseInt("");
+		System.out.println("i = " + i);
+
 	}
+
 
 }
